f8d10627f80f0bd4e813b356e628942196c9bdcb
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-10-20 00:31:16 -0300

    :art: "Feat: Add Header component to pages"

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class NotFound extends Component {
   render() {
     return (
       <div data-testid="page-not-found">
+        <Header />
         Não encontrado
       </div>
     );
