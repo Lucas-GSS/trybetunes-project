660ce759eaeb16317b8835b2d48f9c159a934ba2
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-10-20 12:12:00 -0300

    :sparkles: "Feat: Implements Search Form component"

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchForm extends Component {
+  render() {
+    const { handleChange, handleClick, artist } = this.props;
+    const minimumLength = 2;
+    return (
+      <div>
+        <input
+          className="search-input"
+          data-testid="search-artist-input"
+          type="text"
+          placeholder="Digite o nome do artista"
+          onChange={ handleChange }
+        />
+        <button
+          className="search-button"
+          data-testid="search-artist-button"
+          disabled={ artist.length < minimumLength }
+          type="button"
+          onClick={ handleClick }
+        >
+          Pesquisar
+        </button>
+      </div>
+    );
+  }
+}
+
+SearchForm.propTypes = PropTypes.shape({
+  handleChange: PropTypes.func,
+  handleClick: PropTypes.func,
+  searchText: PropTypes.string,
+}).isRequired;
+
+export default SearchForm;
