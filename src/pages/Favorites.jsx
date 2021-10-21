5e7d0ef464d81f6f81caea747d533a3b0f68edc8
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-10-21 12:54:24 -0300

    :sparkles: "Feat: Implements Favorites page"

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true, favorites: [] };
+  }
+
+  componentDidMount() {
+    this.getFavorites();
+  }
+
+  handleChange = async (_event, song) => {
+    this.setState({ loading: true });
+    await removeSong(song);
+    this.getFavorites();
+    this.setState({ loading: false });
+  }
+
+  getFavorites = async () => {
+    const favoritesList = await getFavoriteSongs();
+    this.setState({ loading: false, favorites: favoritesList });
+  }
+
   render() {
+    const { favorites, loading } = this.state;
+    if (loading) return <Loading />;
     return (
       <div data-testid="page-favorites">
         <Header />
-        Favorites
+        { favorites.map((song) => (
+          <MusicCard
+            key={ song.trackId }
+            favorites={ favorites }
+            song={ song }
+            handleChange={ this.handleChange }
+          />
+        )) }
       </div>
     );
   }
