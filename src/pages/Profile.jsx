1ab48dfad6db52b3a6441c6d08af345b9a9cea8d
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-10-21 16:18:00 -0300

    :sparkles: "Feat: Implements Profile page"

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
-class Profile extends Component {
+class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = { userInfo: {}, loading: false };
+  }
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  getUserInfo = async () => {
+    this.setState({ loading: true });
+    const userInfo = await getUser();
+    this.setState({ userInfo, loading: false });
+  }
+
   render() {
+    const { loading, userInfo: { name, email, description, image } } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Perfil
+        { loading && <Loading /> }
+        <section>
+          <img src={ image } alt={ name } data-testid="profile-image" width="100px" />
+          <h4>Nome:</h4>
+          <span>{ name }</span>
+          <h4>E-mail:</h4>
+          <span>{ email }</span>
+          <h4>Descrição</h4>
+          <span>{ description }</span>
+        </section>
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
